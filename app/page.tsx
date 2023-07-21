5c7c667b97898249bac314434ef9ec09bf53b544
moisestech <m@moises.tech>
2023-07-20 21:03:55 -0400

    test json streaming

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,82 @@
 'use client';
 
+// Return in your response only a valid JSON object in this shape { "id": "id1", "name": "joe", "location": "new york", "age": 30 }
+
 import Image from 'next/image';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { Toaster, toast } from 'react-hot-toast';
 import DropDown, { VibeType } from '../components/DropDown';
-import Footer from '../components/Footer';
-import Github from '../components/GitHub';
 import Header from '../components/Header';
 import { useChat } from 'ai/react';
 
 export default function Page() {
-  const [bio, setBio] = useState('');
-  const [vibe, setVibe] = useState<VibeType>('Professional');
-  const bioRef = useRef<null | HTMLDivElement>(null);
+  const [prompt, setPrompt] = useState('Test');
+  const [generatedJSON, setGeneratedJSON] = useState(null);
+  const [jsonError, setJsonError] = useState(null);
+  const jsonRef = useRef<null | HTMLDivElement>(null);
 
-  const scrollToBios = () => {
-    if (bioRef.current !== null) {
-      bioRef.current.scrollIntoView({ behavior: 'smooth' });
+  const scrollToJSON = () => {
+    if (jsonRef.current !== null) {
+      jsonRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
   const { input, handleInputChange, handleSubmit, isLoading, messages } =
     useChat({
       body: {
-        vibe,
-        bio,
+        prompt
       },
       onResponse() {
-        scrollToBios();
+        scrollToJSON();
       },
     });
 
   const onSubmit = (e: any) => {
-    setBio(input);
+    e.preventDefault();
+    setPrompt(input);
     handleSubmit(e);
   };
 
-  const lastMessage = messages[messages.length - 1];
-  const generatedBios = lastMessage?.role === "assistant" ? lastMessage.content : null;
+  const isValidJSON = (str) => {
+    try {
+      JSON.parse(str);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    const lastMessage = messages[messages.length - 1];
+    let messageContent = lastMessage?.role === "assistant" ? lastMessage.content : null;
+  
+    if (messageContent) {
+      const validJSON = isValidJSON(messageContent);
+  
+      if (validJSON) {
+        let parsedJSON = JSON.parse(messageContent);
+        setGeneratedJSON(messageContent); // <-- set the generatedJSON state here
+        setJsonError(null);
+        console.log(validJSON);
+        console.log(messageContent);
+        console.log(parsedJSON);
+        
+      } else {
+        setGeneratedJSON(null); // <-- reset the generatedJSON state if invalid
+        setJsonError('Invalid JSON');
+        console.log('invalid JSON')
+        console.log(messageContent)
+      }
+    } else {
+      setGeneratedJSON(null); // <-- reset the generatedJSON state if there is no new message
+    }
+  }, [messages]);
 
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
-        <a
-          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/Nutlope/twitterbio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github />
-          <p>Star on GitHub</p>
-        </a>
-        <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate your next Twitter bio using chatGPT
-        </h1>
-        <p className="text-slate-500 mt-5">47,118 bios generated so far.</p>
+        
         <form className="max-w-xl w-full" onSubmit={onSubmit}>
           <div className="flex mt-10 items-center space-x-3">
             <Image
@@ -66,11 +87,7 @@ export default function Page() {
               className="mb-5 sm:mb-0"
             />
             <p className="text-left font-medium">
-              Copy your current bio{' '}
-              <span className="text-slate-500">
-                (or write a few sentences about yourself)
-              </span>
-              .
+              Prompt the AI Model to generate a valid JSON Object
             </p>
           </div>
           <textarea
@@ -79,23 +96,16 @@ export default function Page() {
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
             placeholder={
-              'e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com.'
+              'Generate a valid JSON object in this style: \n{ "id": "id1", "name": "joe", "location": "new york", "age": 30 }'
             }
           />
-          <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">Select your vibe.</p>
-          </div>
-          <div className="block">
-            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
-          </div>
 
           {!isLoading && (
             <button
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
               type="submit"
             >
-              Generate your bio &rarr;
+              Generate JSON &rarr;
             </button>
           )}
           {isLoading && (
@@ -118,42 +128,78 @@ export default function Page() {
         />
         <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
         <output className="space-y-10 my-10">
-          {generatedBios && (
+
+        {jsonError && (
+            <div className="my-5 p-4 bg-red-100 text-red-700 border border-red-400 rounded">
+              <h3 className="font-bold">Error:</h3>
+              <p>{jsonError}</p>
+              <p>Raw response from model:</p>
+              <pre>{generatedJSON}</pre>
+            </div>
+          )}
+
+          {generatedJSON && (
             <>
-              <div>
+              <article>
                 <h2
                   className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
-                  ref={bioRef}
+                  ref={jsonRef}
                 >
-                  Your generated bios
+                  Your generated JSON
                 </h2>
-              </div>
-              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-                {generatedBios
-                  .substring(generatedBios.indexOf('1') + 3)
-                  .split('2.')
-                  .map((generatedBio) => {
-                    return (
-                      <div
-                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
-                        onClick={() => {
-                          navigator.clipboard.writeText(generatedBio);
-                          toast('Bio copied to clipboard', {
-                            icon: '✂️',
-                          });
-                        }}
-                        key={generatedBio}
-                      >
-                        <p>{generatedBio}</p>
-                      </div>
-                    );
-                  })}
-              </div>
+              </article>
+              <article className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+                <div
+                  className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                  onClick={() => {
+                    navigator.clipboard.writeText(generatedJSON);
+                    toast('Bio copied to clipboard', {
+                      icon: '✂️',
+                    });
+                  }}
+                  key={generatedJSON}
+                >
+                  <p>{generatedJSON}</p>
+                </div>
+              </article>
+              <article className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+                <div
+                  className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                  onClick={() => {
+                    navigator.clipboard.writeText(generatedJSON);
+                    toast('Bio copied to clipboard', {
+                      icon: '✂️',
+                    });
+                  }}
+                  key={generatedJSON}
+                >
+                  <pre>{JSON.stringify(JSON.parse(generatedJSON), null, 2)}</pre>
+                </div>
+              </article>
+              <article className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+                <div
+                  className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                  onClick={() => {
+                    navigator.clipboard.writeText(generatedJSON);
+                    toast('Bio copied to clipboard', {
+                      icon: '✂️',
+                    });
+                  }}
+                  key={generatedJSON}
+                >
+                  <span>
+                    {Object.entries(JSON.parse(generatedJSON)).map(([key, value]) => (
+                      <p key={key}>
+                        {key}: {value}
+                      </p>
+                    ))}
+                  </span>
+                </div>
+              </article>
             </>
           )}
         </output>
       </main>
-      <Footer />
     </div>
   );
 }
